6815dec7026e4fc6993212c93503fc675761674d
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-03-25 21:33:30 +0300

    Complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Mercy Wanjiru";
+let excited = "!";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}${excited}`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}${excited}`;
+console.log(shortGreeting);
